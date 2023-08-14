8eef3318ba095267192fee01a6567d9ff1be8602
Jonathan Cobb <jonathan@kyuss.org>
2023-08-14 00:10:58 -0400

    revert MobilettoScanObjectTypeDef type

--- lib/esm/type/model/ScanLockType.d.ts
+++
@@ -1,5 +0,0 @@
-import { MobilettoOrmObject } from "mobiletto-orm-typedef";
-export type ScanLockType = MobilettoOrmObject & {
-    lock: string;
-    owner?: string;
-};
--- src/scripts/generate.ts
+++ src/scripts/generate.ts
@@ -147,7 +147,6 @@ const GEN_TYPES: GenSpec[] = [
   { typedef: DiscoveredAssetTypeDef, generate: GEN_ALL },
   { typedef: DownloadedAssetTypeDef, generate: GEN_ALL },
   { typedef: AnalyzedAssetTypeDef, generate: GEN_ALL },
-  { typedef: MobilettoScanObjectTypeDef, generate: GEN_TYPE },
   { typedef: AuthAccountTypeDef, generate: GEN_TYPE, tsDir: TS_AUTH_TYPE_DIR },
   { typedef: SessionTypeDef, generate: GEN_TYPE, tsDir: TS_AUTH_TYPE_DIR },
   { typedef: UsernameAndPasswordTypeDef, generate: GEN_TYPE, tsDir: TS_AUTH_TYPE_DIR },
